ec44e734d4a3ff67bb109c6fb12c5cb866683801
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-03-31 19:21:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="font-bold text-red-700 text-center">Hello</h1>
+            <button className="btn"> siam</button>
+        </>
+    );
+}
+
+export default App;
